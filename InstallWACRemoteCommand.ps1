f58f5ce5c7c1fbbedb1d953b585c0c1099fc7e17
brunosouzagreen <72158493+brunosouzagreen@users.noreply.github.com>
2022-02-18 18:43:39 -0300

    Update InstallWACRemoteCommand.ps1

--- InstallWACRemoteCommand.ps1
+++ InstallWACRemoteCommand.ps1
@@ -1,7 +1,7 @@
-Start-BitsTransfer -Source https://aka.ms/WACDownload -Destination "$env:USERPROFILE\Downloads\WindowsAdminCenter.msi"
+Start-BitsTransfer -Source https://aka.ms/WACDownload -Destination "C:\Windows\Temp\WindowsAdminCenter.msi"
 
 Write-Host "Install Windows Admin Center" -ForegroundColor Green
-Start-Process msiexec.exe -Wait -ArgumentList "/i $env:USERPROFILE\Downloads\WindowsAdminCenter.msi /qn /L*v log.txt REGISTRY_REDIRECT_PORT_80=1 SME_PORT=443 SSL_CERTIFICATE_OPTION=generate"
+Start-Process msiexec.exe -Wait -ArgumentList "/i C:\Windows\Temp\WindowsAdminCenter.msi /qn /L*v log.txt REGISTRY_REDIRECT_PORT_80=1 SME_PORT=443 SSL_CERTIFICATE_OPTION=generate"
 
 Start-Sleep -Seconds 10
 
